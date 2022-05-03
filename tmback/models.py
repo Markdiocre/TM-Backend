49de60f6733c4b093cb2ba0bcf9d0bd4b224f027
Markdiocre <mark.manuel.bsuiness@gmail.com>
2022-05-03 19:48:09 +0800

    Added product description

--- tmback/models.py
+++ tmback/models.py
@@ -26,6 +26,7 @@ class Product(models.Model):
     company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     name = models.CharField(max_length=30)
+    description = models.TextField(default='')
     price = models.IntegerField()
 
     def __str__(self):
